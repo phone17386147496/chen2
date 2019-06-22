057d2f0190163b9a0fc37e5370e78702176d041d
chenwei <251201429@qq.com>
2019-06-22 20:54:25 +0800

    传到chen2中

---
+++ src/main/java/com/heima/Test.java
@@ -0,0 +1,16 @@
+package com.heima;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("chenwei");
+		
+		int a = 10;
+		System.out.println(a);
+
+		int b = 20;
+        System.out.println("chenxs");
+		System.out.println(b);
+
+
+    }
+}
